b5c656e53b3abb707a794d6b40781cd02618a22c
Wilson Dev <hch.wilson@hynkor.com>
2024-09-06 19:27:49 -0500

    Add Private

---
+++ src/04-private.ts
@@ -0,0 +1,53 @@
+// Class
+export class MyDate {
+  // Properties
+  year: number;
+  month: number;
+  private day: number;
+  // Constructor
+  constructor(year: number, month: number, day: number) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+  // Methods
+  printFormat(): string {
+    const day = this.addPadding(this.day);
+    const month = this.addPadding(this.month);
+    return `${day}/${month}/${this.year}`;
+  }
+
+  private addPadding(value: number){
+    if (value < 10){
+      return `0${value}`;
+    }
+    return `${value}`;
+  }
+
+  add(amount: number, type: 'days' | 'months' | 'years') {
+    if (type === 'days') {
+      this.day += amount;
+    }
+    if (type === 'months') {
+      this.month += amount;
+    }
+    if (type === 'years') {
+      this.year += amount;
+    }
+  }
+
+  getDay(){
+    return this.day;
+  }
+}
+
+const myDate = new MyDate(1993, 7, 10);
+console.log(myDate.printFormat());
+console.log(myDate.getDay());
+
+// Method addPadding en modo private no es accesible desde afuera.
+// myDate.addPadding(12);
+
+// console.log(myDate.day);
+// myDate.day = 12;
+// console.log(myDate.day);
